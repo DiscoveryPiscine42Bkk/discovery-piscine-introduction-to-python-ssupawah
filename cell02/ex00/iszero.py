64aad82700b33df6180c5226d7aac30c3ea091e2
ssupawah <siriwimonsupawaha@gmail.com>
2025-06-17 04:39:43 +0000

    00

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,8 @@
+number = float(input("please enter a numder: "))
+
+if number < 0:
+     print("this number is negative.")
+elif number >0:
+     print("this number is positive.")
+else:
+    print("this number is both positive and negative.")
