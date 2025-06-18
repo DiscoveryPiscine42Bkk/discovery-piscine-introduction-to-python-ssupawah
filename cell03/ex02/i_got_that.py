567a4c384c6520325fbd54e1b9305f5e60481d4c
ssupawah <siriwimonsupawaha@gmail.com>
2025-06-18 04:07:17 +0000

    02

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,8 @@
+while true:
+    user_input + input("say something (type 'STOP' to exit): ")
+
+    if user_input.upper() == "STOP":
+        PRINT("stopping the program.")
+        break
+
+        print(f"I got that: {user_input}")
